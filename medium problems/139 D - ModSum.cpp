842105c02ede0c1bab3eb584a6d534e3f1baab12
saladmence <150103284+saladmence@users.noreply.github.com>
2025-01-21 00:50:25 -0600

    Create 139 D - ModSum.cpp

---
+++ medium problems/139 D - ModSum.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+// yippee
+
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  ll n; cin >> n;
+  cout << (n)*(n+1)/2 - n;
+}
